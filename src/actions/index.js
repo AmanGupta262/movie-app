d7d561ba1e533c940c275cc6aa7953a7e506acce
Aman Gupta <amang5840@gmail.com>
2021-05-15 00:38:12 +0530

    improve actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
-// {
-//     type: "ADD_MOVIES"
-// }
+// actions types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies(movies){
+    return {
+        type: ADD_MOVIES,
+        movies: movies
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
+import React from 'react';
+
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import {addMovies} from '../actions';
 
 class App extends React.Component {
 
   componentDidMount() {
+    const { store } = this.props;
+
     //make api call
     store.subscribe(() => {
       console.log('Updated');
       this.forceUpdate();
     });
     //dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch(addMovies(data));
   }
 
   render() {
